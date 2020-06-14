a4ebce4b2c92a5599196c1843e119f69094ebaf0
Tomas <45800655+lithualien@users.noreply.github.com>
2020-06-14 17:14:44 +0300

    initial commit

---
+++ src/main/java/com/github/lithualien/addvertismentwebsite/AddvertismentWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.addvertismentwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddvertismentWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddvertismentWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lithualien/addvertismentwebsite/AddvertismentWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lithualien.addvertismentwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddvertismentWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
