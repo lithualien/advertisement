58e540a4b74000e55a09eb0d91a94f0ea415ee8c
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-07 15:35:12 +0300

    Bidirectional relationship between Category and SubCategory.

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/models/Category.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/Category.java
@@ -6,7 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -18,4 +20,6 @@ public class Category extends BaseEntity {
 
     private String category;
 
+    @OneToMany(mappedBy = "category")
+    private Set<SubCategory> categories;
 }
--- advertisement-data/src/main/java/com/github/lithualien/advertisement/models/SubCategory.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/SubCategory.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Getter
@@ -19,5 +20,8 @@ public class SubCategory extends BaseEntity {
 
     @Column(name = "sub_category")
     private String subCategory;
+
+    @ManyToOne
+    private Category category;
 
 }
