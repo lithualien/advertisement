6ab0a9de0ffc788e91aa81cbfaadf540fd43f9e7
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-07 14:58:04 +0300

    Adding Category entity.

---
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/Category.java
@@ -0,0 +1,21 @@
+package com.github.lithualien.advertisement.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category extends BaseEntity {
+
+    private String category;
+
+}
