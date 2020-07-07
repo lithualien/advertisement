e32f75d1e7cf4e7c4ce50225f87e71959d9520c0
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-07 15:27:18 +0300

    Adding Sub-Category entity.

---
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/SubCategory.java
@@ -0,0 +1,23 @@
+package com.github.lithualien.advertisement.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sub_categories")
+public class SubCategory extends BaseEntity {
+
+    @Column(name = "sub_category")
+    private String subCategory;
+
+}
