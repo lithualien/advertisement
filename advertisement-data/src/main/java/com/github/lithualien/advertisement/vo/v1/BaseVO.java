e0b59a0ad0df965d124882f93220816226aa05f0
lithualien <tomas.dominauskas@protonmail.com>
2020-07-08 18:38:48 +0300

    Manual equals and hashcode, instead of Lombok. Lombok did not hash the id.

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/vo/v1/BaseVO.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/vo/v1/BaseVO.java
@@ -1,14 +1,30 @@
 package com.github.lithualien.advertisement.vo.v1;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode
 public class BaseVO {
 
     private Long id;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseVO baseVO = (BaseVO) o;
+        return id.equals(baseVO.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
