f5fdd2241f56e319a19004472f941edafa3c15cc
lithualien <tomas.dominauskas@protonmail.com>
2020-07-12 21:58:56 +0300

    Updated SubCategoryRepository

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/repositories/SubCategoryRepository.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/repositories/SubCategoryRepository.java
@@ -1,10 +1,14 @@
 package com.github.lithualien.advertisement.repositories;
 
+import com.github.lithualien.advertisement.models.Category;
 import com.github.lithualien.advertisement.models.SubCategory;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface SubCategoryRepository extends CrudRepository<SubCategory, Long> {
 
-
+    @Query("select case when count(subCategory) > 0 then true else false end "+
+            "from SubCategory subCategory where subCategory.subCategory = :subCategory and subCategory.category = :category")
+    Boolean findIfSubCategoryExists(String subCategory, Category category);
 
 }
