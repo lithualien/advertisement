d8c01fb3072b9d0139ba620dbde37754b11d933f
lithualien <tomas.dominauskas@protonmail.com>
2020-07-17 13:53:53 +0300

    Adding Type entity and SQL

---
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/Type.java
@@ -0,0 +1,21 @@
+package com.github.lithualien.advertisement.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "types")
+public class Type extends BaseEntity {
+
+    private String type;
+
+}
