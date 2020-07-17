c5c9803e89f0320bb129c0bcabfdc83efd70a77c
lithualien <tomas.dominauskas@protonmail.com>
2020-07-17 13:56:35 +0300

    Adding Advertisement superclass.

---
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/superclass/Advertisement.java
@@ -0,0 +1,32 @@
+package com.github.lithualien.advertisement.models.superclass;
+
+import com.github.lithualien.advertisement.models.BaseEntity;
+import com.github.lithualien.advertisement.models.Type;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import java.time.LocalDate;
+
+@MappedSuperclass
+public abstract class Advertisement extends BaseEntity {
+
+    private String article;
+
+    private Double price;
+
+    private String description;
+
+    @OneToOne
+    private Type type;
+
+    @Column(name = "created_at")
+    private LocalDate createDate;
+
+    @Column(name = "updated_at")
+    private LocalDate updateDate;
+
+    @Column(name = "sold_at")
+    private LocalDate soldDate;
+
+}
