ce0e3de84ee8b65a4473604546ccac4e37483bdc
lithualien <tomas.dominauskas@protonmail.com>
2020-07-17 14:05:54 +0300

    Phone advertisement entity.

---
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/PhoneAdvertisement.java
@@ -0,0 +1,29 @@
+package com.github.lithualien.advertisement.models;
+
+import com.github.lithualien.advertisement.models.superclass.Advertisement;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "phone_advertisements")
+public class PhoneAdvertisement extends Advertisement {
+
+    private String manufacturer;
+
+    private String model;
+
+    private String color;
+
+    private String memory;
+
+
+}
