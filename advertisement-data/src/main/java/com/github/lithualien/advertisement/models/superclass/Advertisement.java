b36b6580c30de7f0d5f11f4ab6746824efca860e
lithualien <tomas.dominauskas@protonmail.com>
2020-07-17 15:01:20 +0300

    Updating Advertisement.

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/models/superclass/Advertisement.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/models/superclass/Advertisement.java
@@ -4,18 +4,22 @@ import com.github.lithualien.advertisement.models.BaseEntity;
 import com.github.lithualien.advertisement.models.Type;
 
 import javax.persistence.Column;
+import javax.persistence.Lob;
 import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToOne;
 import java.time.LocalDate;
 
 @MappedSuperclass
-public abstract class Advertisement extends BaseEntity {
+public class Advertisement extends BaseEntity {
 
     private String article;
 
     private Double price;
 
     private String description;
+
+    @Lob
+    private Byte[] images;
 
     @OneToOne
     private Type type;
