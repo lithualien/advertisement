c4824ce1717b22d97b1db7cbb630861c70c80074
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-25 21:40:06 +0300

    updated computer advertisements

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/services/ComputerAdvertisementService.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/services/ComputerAdvertisementService.java
@@ -1,17 +1,13 @@
 package com.github.lithualien.advertisement.services;
 
-import com.github.lithualien.advertisement.models.ComputerAdvertisement;
 import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementVO;
+import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementWithImageVO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Set;
-
 public interface ComputerAdvertisementService {
 
-    Page<ComputerAdvertisement> all(Pageable pageable);
-
-    ComputerAdvertisementVO findAdvertisement(String category);
+    Page<ComputerAdvertisementWithImageVO> all(Pageable pageable);
 
     ComputerAdvertisementVO save(ComputerAdvertisementVO computerAdvertisementVO);
 
--- advertisement-data/src/main/java/com/github/lithualien/advertisement/services/impl/ComputerAdvertisementServiceImpl.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/services/impl/ComputerAdvertisementServiceImpl.java
@@ -1,19 +1,17 @@
 package com.github.lithualien.advertisement.services.impl;
 
+import com.github.lithualien.advertisement.converters.DozerConverter;
 import com.github.lithualien.advertisement.models.ComputerAdvertisement;
 import com.github.lithualien.advertisement.repositories.ComputerAdvertisementRepository;
-//import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementVO;
+import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementVO;
 import com.github.lithualien.advertisement.services.ComputerAdvertisementService;
+import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementWithImageVO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
 @Service
-public class ComputerAdvertisementServiceImpl implements ComputerAdvertisementService {
+public class ComputerAdvertisementServiceImpl extends AbstractAdvertisementService<ComputerAdvertisement> implements ComputerAdvertisementService {
 
     private final ComputerAdvertisementRepository computerAdvertisementRepository;
 
@@ -22,32 +20,29 @@ public class ComputerAdvertisementServiceImpl implements ComputerAdvertisementSe
     }
 
     @Override
-    public Page<ComputerAdvertisement> all(Pageable pageable) {
-
-        return computerAdvertisementRepository.findAll(pageable);
-//        return StreamSupport.stream(
-//                computerAdvertisementRepository.findAll().spliterator(), false
-//        )
-//                .collect(Collectors.toSet());
+    public Page<ComputerAdvertisementWithImageVO> all(Pageable pageable) {
+        Page<ComputerAdvertisement> computerAdvertisements = super.all(pageable, computerAdvertisementRepository);
+        return super
+                .all(pageable, computerAdvertisementRepository)
+                .map(this::convert);
     }
 
-//    @Override
-//    public ComputerAdvertisementVO findAdvertisement(String category) {
-//        return null;
-//    }
-//
-//    @Override
-//    public ComputerAdvertisementVO save(ComputerAdvertisementVO computerAdvertisementVO) {
-//        return null;
-//    }
-//
-//    @Override
-//    public ComputerAdvertisementVO update(ComputerAdvertisementVO computerAdvertisementVO) {
-//        return null;
-//    }
+    @Override
+    public ComputerAdvertisementVO save(ComputerAdvertisementVO computerAdvertisementVO) {
+        return null;
+    }
+
+    @Override
+    public ComputerAdvertisementVO update(ComputerAdvertisementVO computerAdvertisementVO) {
+        return null;
+    }
 
     @Override
     public void delete(Long id) {
 
+    }
+
+    private ComputerAdvertisementWithImageVO convert(ComputerAdvertisement computerAdvertisement) {
+        return DozerConverter.parseObject(computerAdvertisement, ComputerAdvertisementWithImageVO.class);
     }
 }
