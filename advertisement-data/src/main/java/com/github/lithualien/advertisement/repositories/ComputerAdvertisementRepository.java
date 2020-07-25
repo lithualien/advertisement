a03d57da4ae7b90a0f3a0ca2dab571574e985d0e
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-25 22:53:39 +0300

    added ComputerAdvertisementRepository and ComputerAdvertisementController

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/repositories/ComputerAdvertisementRepository.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/repositories/ComputerAdvertisementRepository.java
@@ -1,14 +1,8 @@
 package com.github.lithualien.advertisement.repositories;
 
 import com.github.lithualien.advertisement.models.ComputerAdvertisement;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface ComputerAdvertisementRepository extends CrudRepository<ComputerAdvertisement, Long> {
+public interface ComputerAdvertisementRepository extends AdvertisementRepository<ComputerAdvertisement> {
 
-    Page<ComputerAdvertisement> findAll(Pageable pageable);
 
 }
--- advertisement-web/src/main/java/com/github/lithualien/advertisement/controllers/ComputerAdvertisementController.java
+++ advertisement-web/src/main/java/com/github/lithualien/advertisement/controllers/ComputerAdvertisementController.java
@@ -2,35 +2,36 @@ package com.github.lithualien.advertisement.controllers;
 
 import com.github.lithualien.advertisement.models.ComputerAdvertisement;
 import com.github.lithualien.advertisement.services.ComputerAdvertisementService;
+import com.github.lithualien.advertisement.vo.v1.AdvertisementVO;
+import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementVO;
+import com.github.lithualien.advertisement.vo.v1.ComputerAdvertisementWithImageVO;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("api/computers/v1")
 public class ComputerAdvertisementController {
 
     private final ComputerAdvertisementService computerAdvertisementService;
-    private final PagedResourcesAssembler<ComputerAdvertisement> assembler;
+    private final PagedResourcesAssembler<ComputerAdvertisementWithImageVO> assembler;
 
-    public ComputerAdvertisementController(ComputerAdvertisementService computerAdvertisementService, PagedResourcesAssembler<ComputerAdvertisement> assembler) {
+    public ComputerAdvertisementController(ComputerAdvertisementService computerAdvertisementService,
+                                           PagedResourcesAssembler<ComputerAdvertisementWithImageVO> assembler) {
         this.computerAdvertisementService = computerAdvertisementService;
         this.assembler = assembler;
     }
 
     @GetMapping
-    public ResponseEntity<?> advertisements(
+    public ResponseEntity<?> getAdvertisements(
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "size", defaultValue = "20") Integer size,
-            @RequestParam(value = "sort", defaultValue = "desc") String sort
-    ) {
+            @RequestParam(value = "sort", defaultValue = "desc") String sort ) {
 
         String[] array = sort.split(",");
 
@@ -39,5 +40,23 @@ public class ComputerAdvertisementController {
         Pageable pageable = PageRequest.of(page, size, Sort.by(sortOrder,  "id"));
 
         return new ResponseEntity<>(assembler.toModel(computerAdvertisementService.all(pageable)), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<ComputerAdvertisementVO> saveAdvertisement(
+            @Valid @RequestBody ComputerAdvertisementVO computerAdvertisementVO) {
+        return new ResponseEntity<>(computerAdvertisementService.save(computerAdvertisementVO), HttpStatus.OK);
+    }
+
+    @PutMapping
+    public ResponseEntity<ComputerAdvertisementVO> updateAdvertisement(
+            @Valid @RequestBody ComputerAdvertisementVO computerAdvertisementVO) {
+        return new ResponseEntity<>(computerAdvertisementService.update(computerAdvertisementVO), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable("id") Long id) {
+        computerAdvertisementService.delete(id);
     }
 }
