89035dd4418d48ccf675a2e6952d326872668c3b
Tomas <45800655+lithualien@users.noreply.github.com>
2020-07-26 23:23:06 +0300

    updated SubCategoryRepository

--- advertisement-data/src/main/java/com/github/lithualien/advertisement/repositories/SubCategoryRepository.java
+++ advertisement-data/src/main/java/com/github/lithualien/advertisement/repositories/SubCategoryRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SubCategoryRepository extends CrudRepository<SubCategory, Long> {
 
     @Query("select case when count(subCategory) > 0 then true else false end "+
             "from SubCategory subCategory where subCategory.subCategory = :subCategory and subCategory.category = :category")
     Boolean findIfSubCategoryExists(String subCategory, Category category);
+
+    Optional<SubCategory> findBySubCategory(String subCategory);
 
 }
